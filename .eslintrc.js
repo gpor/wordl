b55da9b2ece858baac2f11a8b9c55779d9d97f70
lee <lee@dcodegroup.com>
2022-02-12 20:25:22 +1100

    create-react-app and clean up

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+  ],
+  "rules": {
+    "no-unused-vars": "off",
+    "no-empty": "off",
+    "react/react-in-jsx-scope": "off",
+    "comma-dangle": ["error", {
+      "objects": "always-multiline",
+      "arrays": "always-multiline",
+    }],
+    "indent": [2, 2, { "SwitchCase": 1 }],
+  },
+}
